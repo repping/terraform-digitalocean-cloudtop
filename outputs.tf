45b881e323cbe3450287de3edc3c8f9de764a108
repping <r.eppingbroek@outlook.com>
2022-10-26 18:04:19 +0200

    extra output cmd for quick connecting via the cloudtop dns

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "how_to_connect_dns" {
-  value = "ssh -i ${module.digitalocean_droplet.droplet_ssh_key} root@${var.project_name}.${var.cloudflare_zone}"
+  value = "ssh-keygen -R ${var.project_name}.${var.cloudflare_zone}; ssh -i ${module.digitalocean_droplet.droplet_ssh_key} root@${var.project_name}.${var.cloudflare_zone}"
 }
 output "how_to_connect_ip" {
   value = "ssh -i ${module.digitalocean_droplet.droplet_ssh_key} root@${module.digitalocean_droplet.droplet_ip}"

673de7139970d16d27b6f09a771a170511b0f2ba
repping <r.eppingbroek@outlook.com>
2022-11-02 17:28:12 +0100

    added region var and vars for CI

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "Region for the DO resources to be deployed in."
+  type        = string
+  default     = "ams3"
+}
 variable "do_token" {
   description = "The Digital Ocean token."
   type        = string
@@ -32,6 +37,16 @@ variable "cloudflare_token" {
 }
 variable "persistent_home_volume_name" {
   description = "Name of the digitalocean volume to attach as the volume where the /home folder will be mounted. This is to keep personal data when the droplet is destroyed to save cost."
+  type        = string
+  default     = ""
+}
+variable "persistent_home_volume_from_snapshot" {
+  description = "Boolean to enable the creation of the persistent home volume from an existing snapshot."
+  type        = bool
+  default     = false
+}
+variable "persistent_volume_snapshot_name" {
+  description = "Name of the snapshot to use for the creation of the persistent home volume. Only applicable when persistent_home_volume_from_snapshot is set to true."
   type        = string
   default     = ""
 }

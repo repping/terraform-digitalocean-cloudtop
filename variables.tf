e1e89c92ac4ea987b8f804649c3766bddfb5f43f
repping <r.eppingbroek@outlook.com>
2022-11-03 14:29:40 +0100

    ci pipeline fix

--- variables.tf
+++ variables.tf
@@ -22,19 +22,19 @@ variable "cloudflare_zone" {
   description = "Name of the domain used in cloudflare."
   type        = string
   default     = ""
-  # validation {
-  #   condition     = var.cloudflare_zone != ""
-  #   error_message = "Cloudflare zone/domain not set."
-  # }
+  validation {
+    condition     = var.cloudflare_zone != ""
+    error_message = "Cloudflare zone/domain not set."
+  }
 }
 variable "cloudflare_token" {
   description = "API token for Cloudflare."
   type        = string
   default     = ""
-  # validation {
-  #   condition     = var.cloudflare_token != ""
-  #   error_message = "Cloudflare API token not set."
-  # }
+  validation {
+    condition     = var.cloudflare_token != ""
+    error_message = "Cloudflare API token not set."
+  }
 }
 variable "persistent_home_volume_name" {
   description = "Name of the digitalocean volume to attach as the volume where the /home folder will be mounted. This is to keep personal data when the droplet is destroyed to save cost."
